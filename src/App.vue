b78273a29845fbfbb03627404b38e4abc6574648
Marcio Willian <willian_mw.cc@hotmail.com>
2021-03-14 21:15:41 -0300

    Implementando CSS

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>Problema de montyhall</h1>
+    <Door number="1" :hasGift="true" />
+  </div>
+</template>
+
+<script>
+import Door from './components/Door'
+
+export default {
+  name: 'App',
+  components: {
+    Door,
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+  font-family: 'Montserrat', 'sans-serif';
+}
+
+body {
+  color: #FFF;
+  background: linear-gradient(to right,  rgb(21, 153, 87), rgb(21, 87, 153));
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1 {
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+
+</style>
